dd2771c42f113ade31ac6e2490dd4f070bb7c9f3
Jenrikk <henrix427@gmail.com>
2021-10-18 14:34:34 +0200

    back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre:string = 'Capitán América';
+
+  personajes:string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki'];
+
+  PI:number = Math.PI;
+
+  porcentaje:number = 0.224;
+
+  salario:number = 3245;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
